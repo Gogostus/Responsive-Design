b1fce13cc9e7c8d1d656991d3eee79e2a8b7afa2
Roger Tschümperlin <>
2024-06-07 13:57:24 +0200

    eine einfache JQuery funktion eingefügt, zum Test

---
+++ Matildas_Pandas/scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var h2Elements = document.getElementsByTagName("h2");
+    var isWhite = false;
+    for (var i = 0; i < h2Elements.length; i++) {
+        h2Elements[i].addEventListener("click", function() {
+            if (isWhite) {
+                document.body.style.backgroundColor = "black";
+                document.body.style.color = "white";
+                isWhite = false;
+            } else {
+                document.body.style.backgroundColor = "white";
+                document.body.style.color = "black";
+                isWhite = true;
+            }
+            
+        });    } // Add the closing curly brace here
+});
